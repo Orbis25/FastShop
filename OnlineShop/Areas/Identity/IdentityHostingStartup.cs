ca591d5958a50fd96cb72654a073bf8cabfd645e
orbis25 <orbisalonzo@gmail.com>
2019-08-01 01:09:21 -0400

    first commit

---
+++ OnlineShop/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+
+
+[assembly: HostingStartup(typeof(OnlineShop.Areas.Identity.IdentityHostingStartup))]
+namespace OnlineShop.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
