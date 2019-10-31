6fd7bf09328e4e2c00b02731c779f881b879e0b2
orbis25 <orbisalonzo@gmail.com>
2019-10-30 22:13:07 -0400

    adding data seed to create user admin

--- Model/DataSeeding/DataSeeder.cs
+++ Model/DataSeeding/DataSeeder.cs
@@ -3,10 +3,13 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.Extensions.DependencyInjection;
+using Model.Models;
 using OnlineShop.Data;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Model.DataSeeding
 {
@@ -19,7 +22,7 @@ namespace Model.DataSeeding
             {
 
                 ApplicationDbContext _context = appScoped.ServiceProvider.GetService<ApplicationDbContext>();
-
+                UserManager<ApplicationUser> _userManager = appScoped.ServiceProvider.GetService<UserManager<ApplicationUser>>();
                 if (!_context.Roles.Any())
                 {
                     var listRoles = new List<IdentityRole> { 
@@ -30,6 +33,19 @@ namespace Model.DataSeeding
                     _context.AddRange(listRoles);
                     _context.SaveChanges();
                 }
+                if (!_context.ApplicationUsers.Any())
+                {
+                    var user = new ApplicationUser { UserName = "admin@admin.com", Email = "admin@admin.com", Address = "sample", PhoneNumber = "000000000", FullName = "admin" };
+                    _userManager.CreateAsync(user, "admin1234");
+                    _context.Add(user);
+                    _context.SaveChanges();
+
+                    var role = _context.Roles.FirstOrDefault(x => x.Name.Equals("admin"));
+                    _context.UserRoles.Add(new IdentityUserRole<string> { RoleId = role.Id , UserId = user.Id });
+                    _context.SaveChanges();
+
+                }
+
             }
 
         }
